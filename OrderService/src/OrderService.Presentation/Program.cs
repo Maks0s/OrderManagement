528bb01691bcfe1295ac40f634658fddf74e0aa9
Maks0s <maks0sprogram@gmail.com>
2024-09-24 16:17:20 +0300

    feat: Build Clean Architecture skeleton
    - Create layers for usability and abstraction
    - Instead of directly injecting services in 'Program.cs' in 'builder.Services', replaced it with injecting services in the 'DependencyInjection' classes of the app layers via '.Add[LayerName]', and after using '.Add[LayerName]' on 'builder.Services' in 'Program.cs'

--- OrderService/src/OrderService.Presentation/Program.cs
+++ OrderService/src/OrderService.Presentation/Program.cs
@@ -1,15 +1,15 @@
+using OrderService.Presentation;
+using OrderService.Application;
+using OrderService.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddApplication();
+builder.Services.AddInfrastructure(builder.Configuration);
+builder.Services.AddPresentation();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
