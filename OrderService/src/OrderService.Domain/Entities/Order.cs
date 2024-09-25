a8cb3d6a47866f0f8017a796b97f61108160cc85
Maks0s <maks0sprogram@gmail.com>
2024-09-25 17:38:06 +0300

    feat[OrderService.Domain]: Add domain model 'Order'

---
+++ OrderService/src/OrderService.Domain/Entities/Order.cs
@@ -0,0 +1,10 @@
+﻿namespace OrderService.Domain.Entities
+{
+    public class Order
+    {
+        public Guid Id { get; set; }
+        public Guid CustomerId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int ProductQuantity { get; set; }
+    }
+}
