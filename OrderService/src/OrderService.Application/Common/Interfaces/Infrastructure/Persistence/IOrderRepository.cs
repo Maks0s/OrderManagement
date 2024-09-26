a6507614ddcd8184065d0af4c18d77d08cc2a129
Maks0s <maks0sprogram@gmail.com>
2024-09-26 10:52:08 +0300

    feat[OrderService.Application]: Create 'IOrderRepository'
    - Create 'AddOrderAsync' method signature
    - Create 'GetOrderAsync' method signature

---
+++ OrderService/src/OrderService.Application/Common/Interfaces/Infrastructure/Persistence/IOrderRepository.cs
@@ -0,0 +1,11 @@
+﻿using OrderService.Domain.Entities;
+
+namespace OrderService.Application.Common.Interfaces.Infrastructure.Persistence
+{
+    public interface IOrderRepository
+    {
+        public Task<Order?> AddOrderAsync(Order orderToAdd);
+
+        public ValueTask<Order?> GetOrderAsync(Guid orderId);
+    }
+}
