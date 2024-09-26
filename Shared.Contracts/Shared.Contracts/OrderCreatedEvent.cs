9a261572dd9c39c5f439fda3b84cfe6e61af831f
Maks0s <maks0sprogram@gmail.com>
2024-09-26 17:02:50 +0300

    feat[Shared.Contracts]: Create 'OrderCreatedEvent'
    Create shared entity 'OrderCreatedEvent' for communication between microservices via RabbitMQ

---
+++ Shared.Contracts/Shared.Contracts/OrderCreatedEvent.cs
@@ -0,0 +1,10 @@
+﻿namespace Shared.Contracts
+{
+    public record OrderCreatedEvent
+    {
+        public Guid Id { get; set; }
+        public Guid CustomerId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int ProductQuantity { get; set; }
+    }
+}
