5c1b7172844b8c82eb79043ccb4cad355b1be5dc
Maks0s <maks0sprogram@gmail.com>
2024-09-26 17:07:07 +0300

    feat[OrderService.Application]: Create 'AddOrderCommand' and its handling
    - Create 'AddOrderCommand' and 'AddOrderCommandHandler'
    - Create custom error for handling adding data issues: 'Errors.ServerDataManipulation.NotAdded()'
    - Add logic for publishing the 'OrderCreatedEvent' event via MassTransit's 'IPublishEndpoint' in connection with RabbitMQ

---
+++ OrderService/src/OrderService.Application/Orders/CQRS/Commands/Add/AddOrderCommand.cs
@@ -0,0 +1,10 @@
+﻿using OrderService.Application.Common.Interfaces.Application.CQRS;
+using OrderService.Domain.Entities;
+
+namespace OrderService.Application.Orders.CQRS.Commands.Add
+{
+    public record AddOrderCommand(
+            Guid CustomerId,
+            int ProductQuantity
+        ) : ICommand<Order?>;
+}
---
+++ OrderService/src/OrderService.Application/Orders/CQRS/Commands/Add/AddOrderCommandHandler.cs
@@ -0,0 +1,60 @@
+﻿using ErrorOr;
+using MassTransit;
+using OrderService.Application.Common.AppErrors;
+using OrderService.Application.Common.Interfaces.Application.CQRS;
+using OrderService.Application.Common.Interfaces.Infrastructure.Persistence;
+using OrderService.Domain.Entities;
+using Shared.Contracts;
+
+namespace OrderService.Application.Orders.CQRS.Commands.Add
+{
+    public class AddOrderCommandHandler
+        : ICommandHandler<AddOrderCommand, Order?>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly IPublishEndpoint _publishEndpoint;
+
+        public AddOrderCommandHandler(
+                IOrderRepository orderRepository, 
+                IPublishEndpoint publishEndpoint
+            )
+        {
+            _orderRepository = orderRepository;
+            _publishEndpoint = publishEndpoint;
+        }
+
+        public async Task<ErrorOr<Order?>> Handle(
+                AddOrderCommand command, 
+                CancellationToken cancellationToken
+            )
+        {
+            var orderToAdd = new Order()
+            {
+                Id = Guid.NewGuid(),
+                CustomerId = command.CustomerId,
+                OrderDate = DateTime.UtcNow,
+                ProductQuantity = command.ProductQuantity
+            };
+
+            var addedOrder =
+                await _orderRepository.AddOrderAsync(orderToAdd);
+
+            if (addedOrder is null)
+            {
+                return Errors.ServerDataManipulation.NotAdded();
+            }
+
+            await _publishEndpoint.Publish(
+                new OrderCreatedEvent()
+                {
+                    Id = addedOrder.Id,
+                    CustomerId = addedOrder.CustomerId,
+                    OrderDate = addedOrder.OrderDate,
+                    ProductQuantity = addedOrder.ProductQuantity
+                }
+            );
+
+            return addedOrder;
+        }
+    }
+}
