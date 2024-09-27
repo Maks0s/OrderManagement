7df5e2c4eed01b81f4b766edeb969ea473e065f1
Maks0s <maks0sprogram@gmail.com>
2024-09-27 14:36:28 +0300

    feat[OrderService.Application]: Creat 'AddOrderCommandValidator'

---
+++ OrderService/src/OrderService.Application/Orders/CQRS/Commands/Add/AddOrderCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+
+namespace OrderService.Application.Orders.CQRS.Commands.Add
+{
+    public class AddOrderCommandValidator
+        : AbstractValidator<AddOrderCommand>
+    {
+        public AddOrderCommandValidator()
+        {               
+            RuleFor(adc => adc.ProductQuantity)
+                .ExclusiveBetween(0, 10000);
+        }
+    }
+}
