f42bff61cea7f1e643b92456852754e7c915e845
Maks0s <maks0sprogram@gmail.com>
2024-10-01 10:08:12 +0300

    feat[NotificationService.Infrastructure]: Set up and configure MassTransit with RabbitMQ

--- NotificationService/src/NotificationService.Infrastructure/DependencyInjection.cs
+++ NotificationService/src/NotificationService.Infrastructure/DependencyInjection.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using MassTransit;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
+using NotificationService.Infrastructure.RabbitMq.Common;
 
 namespace NotificationService.Infrastructure
 {
@@ -10,6 +13,38 @@ namespace NotificationService.Infrastructure
                 IConfiguration configuration
             )
         {
+            services.AddMassTransitConfigurations(configuration);
+
+            return services;
+        }
+
+        private static IServiceCollection AddMassTransitConfigurations(
+               this IServiceCollection services,
+               IConfiguration configuration
+           )
+        {
+            var rabbitMqConfig = new RabbitMqConfig();
+            configuration.Bind(RabbitMqConfig.SectionName, rabbitMqConfig);
+            services.AddSingleton(Options.Create(rabbitMqConfig));
+
+            services.AddMassTransit(busConfigurator =>
+            {
+                busConfigurator.SetKebabCaseEndpointNameFormatter();
+
+                busConfigurator.AddConsumers(typeof(DependencyInjection).Assembly);
+
+                busConfigurator.UsingRabbitMq((context, configurator) =>
+                {
+                    configurator.Host(rabbitMqConfig.Host, "/", host =>
+                    {
+                        host.Username(rabbitMqConfig.Username);
+                        host.Password(rabbitMqConfig.Password);
+                    });
+
+                    configurator.ConfigureEndpoints(context);
+                });
+            });
+
             return services;
         }
     }
---
+++ NotificationService/src/NotificationService.Infrastructure/RabbitMq/Common/RabbitMqConfig.cs
@@ -0,0 +1,10 @@
+﻿namespace NotificationService.Infrastructure.RabbitMq.Common
+{
+    public class RabbitMqConfig
+    {
+        public const string SectionName = "RabbitMqConfig";
+        public string Host { get; set; } = default!;
+        public string Username { get; set; } = default!;
+        public string Password { get; set; } = default!;
+    }
+}
